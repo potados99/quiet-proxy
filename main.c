6b08bc6e6ffdd0acc835a1c5b2c0440fb280c349
potados99 <potados99@gmail.com>
2023-09-29 13:41:18 +0900

    chore: libliquid 안녕

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include "fec.h"
 #include "sndfile.h"
 #include "portaudio.h"
+#include "liquid.h";
 
 int main() {
     printf("Hello, World!\n");
@@ -10,6 +11,7 @@ int main() {
     printf("libfec is %p\n", create_viterbi27(9));
     printf("libsndfile is %s\n", sf_version_string());
     printf("portaudio is %s\n", Pa_GetVersionText());
+    printf("libliquid is %s\n", liquid_version);
 
     return 0;
 }
