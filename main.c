9c4615fc779f06cc8be56fb6b4a942069450b709
potados99 <potados99@gmail.com>
2023-09-29 13:17:09 +0900

    chore: portaudio 안녕

--- main.c
+++ main.c
@@ -2,12 +2,14 @@
 #include "jansson.h"
 #include "fec.h"
 #include "sndfile.h"
+#include "portaudio.h"
 
 int main() {
     printf("Hello, World!\n");
     printf("Jansson is %s\n", jansson_version_str());
     printf("libfec is %p\n", create_viterbi27(9));
     printf("libsndfile is %s\n", sf_version_string());
+    printf("portaudio is %s\n", Pa_GetVersionText());
 
     return 0;
 }
