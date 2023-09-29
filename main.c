ddd5aff3282b259cd6db530ce80ab07836ee33c6
potados99 <potados99@gmail.com>
2023-09-29 12:23:37 +0900

    fix: libfec을 사용하기 위해 target_include_directories 추가

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include "jansson.h"
+#include "fec.h"
 
 int main() {
     printf("Hello, World!\n");
     printf("Jansson is %s\n", jansson_version_str());
+    printf("libfec is %p\n", create_viterbi27(9));
 
     return 0;
 }
